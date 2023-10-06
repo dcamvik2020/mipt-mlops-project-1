e7ade44178ad7eebcda582a9f4f6e83490743847
Melnikov Victor <vik-melnikov2001@mail.ru>
2023-10-06 22:35:05 +0300

    pre-commit config added, bugs fixed

--- mipt_mlops_project_1/_main.py
+++ mipt_mlops_project_1/_main.py
@@ -1,79 +1,84 @@
 import os
 import sys
 
-from utils.data import save_data, load_data
-from utils.pipeline import train_model, eval_model
-
+from utils.data import load_data, save_data
 from utils.logging import logging_print
+from utils.pipeline import eval_model, train_model
+
 
 def main():
     while True:
         logging = input("Enable logging (yes/no): ")
         if logging in {"yes", "no"}:
             break
-    
+
     data_dir = input("Save data to directory: ")
     if data_dir == "":
-        data_dir = './data/'
-        
+        data_dir = "./data/"
+
     data_file = input("Save data to file: ")
     if data_file == "":
         data_file = "tmp_data.csv"
-        
+
     model_dir = input("Save model to directory: ")
     if model_dir == "":
-        model_dir = './models/'
-        
+        model_dir = "./models/"
+
     model_file = input("Save model to file: ")
     if model_file == "":
         model_file = "catboost.pkl"
-    
-    logging = (logging == "yes")
+
+    logging = logging == "yes"
     # logging_low = 1 - logging # if logging, only high-level logging
     logging_low = False
-    
-    #______________________________________
-    
+
+    # ______________________________________
+
     logging_print(logging=logging, line="STEP 1 : load & save data ... ", end="")
-    
+
     save_data(path_to_save=data_dir, fname=data_file, logging=logging_low)
-    data = load_data(load_path=data_dir, fname=data_file, logging=logging_low)    
-    
+    data = load_data(load_path=data_dir, fname=data_file, logging=logging_low)
+
     # dms = data_memory_size
     dms = data.memory_usage(index=True, deep=True).sum()
-    gb = dms // (1024 ** 3)
-    mb = (dms % (1024 ** 3)) // (1024 ** 2)
-    kb = (dms % (1024 ** 2)) // 1024
+    gb = dms // (1024**3)
+    mb = (dms % (1024**3)) // (1024**2)
+    kb = (dms % (1024**2)) // 1024
     memory_str = f"{gb} GB, {mb} MB, {kb} KB"
 
     logging_print(logging=logging, line="OK")
     logging_print(logging=logging, line="Data shape:" + str(data.shape))
     logging_print(logging=logging, line="Data memory usage:" + memory_str)
     logging_print(logging=logging, line="-----------------------------------------")
-    
-    #______________________________________
-    
+
+    # ______________________________________
+
     logging_print(logging=logging, line="STEP 2 : train & save model ... ", end="")
-    model, data_trn, data_tst = train_model(model_dirname=model_dir, model_fname=model_file, logging=logging_low)
+    model, data_trn, data_tst = train_model(
+        model_dirname=model_dir, model_fname=model_file, logging=logging_low
+    )
     logging_print(logging=logging, line="OK")
     logging_print(logging=logging, line="-----------------------------------------")
-    
-    #______________________________________
-    
+
+    # ______________________________________
+
     logging_print(logging=logging, line="STEP 3 : eval model on test data ... ", end="")
-    scores_trn, scores_tst = eval_model(data_trn, data_tst, model_dirname=model_dir, model_fname=model_file)
+    scores_trn, scores_tst = eval_model(
+        data_trn, data_tst, model_dirname=model_dir, model_fname=model_file
+    )
 
     logging_print(logging=logging, line="OK")
-    logging_print(logging=logging, line='Train:' + str(scores_trn))
-    logging_print(logging=logging, line='Test:' + str(scores_tst))
-    
+    logging_print(logging=logging, line="Train:" + str(scores_trn))
+    logging_print(logging=logging, line="Test:" + str(scores_tst))
+
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("Current working directory:", os.getcwd())
-    if os.getcwd().split('/')[-1] != 'mipt-mlops-course':
-        print("Move to project directory...", end='')
-        os.chdir('./mipt-mlops-course')
+    if os.getcwd().split("/")[-1] != "mipt-mlops-course":
+        print("Move to project directory...", end="")
+        os.chdir("./mipt-mlops-course")
         print("OK")
         print("Current working directory:", os.getcwd())
 
--- mipt_mlops_project_1/infer.py
+++ mipt_mlops_project_1/infer.py
@@ -1,12 +1,12 @@
 import os
+import pickle as pkl
 import sys
 
 import pandas as pd
-import pickle as pkl
-
-from utils.data import save_data, load_data
-from utils.pipeline import train_model, eval_model
+from utils.data import load_data
 from utils.logging import logging_print
+from utils.pipeline import eval_model
+
 
 def main():
     """
@@ -14,19 +14,19 @@ def main():
         logging = input("Enable logging (yes/no): ")
         if logging in {"yes", "no"}:
             break
-    
+
     data_dir = input("Save data to directory: ")
     if data_dir == "":
         data_dir = './data/'
-        
+
     data_file = input("Save data to file: ")
     if data_file == "":
         data_file = "tmp_data.csv"
-        
+
     model_dir = input("Save model to directory: ")
     if model_dir == "":
         model_dir = './models/'
-        
+
     model_file = input("Save model to file: ")
     if model_file == "":
         model_file = "catboost.pkl"
@@ -38,47 +38,51 @@ def main():
     model_dir = "./models/"
     model_file = "model.pkl"
 
-    
-    logging = (logging == "yes")
+    logging = logging == "yes"
     # logging_low = 1 - logging # if logging, only high-level logging
     logging_low = False
-    
-    #______________________________________
-    
+
+    # ______________________________________
+
     logging_print(logging=logging, line="STEP 1 : load data ... ", end="")
-    
-    data_trn, data_tst = load_data(load_path=data_dir, fname=data_file, logging=logging_low)    
-   
+
+    data_trn, data_tst = load_data(
+        load_path=data_dir, fname=data_file, logging=logging_low
+    )
+
     logging_print(logging=logging, line="OK")
     logging_print(logging=logging, line="-----------------------------------------")
-    
-    #______________________________________
-    
+
+    # ______________________________________
+
     logging_print(logging=logging, line="STEP 2 : load model ... ", end="")
     with open(model_dir + model_file, "rb") as f:
         model = pkl.load(f)
     logging_print(logging=logging, line="OK")
     logging_print(logging=logging, line="-----------------------------------------")
-    
-    #______________________________________
-    
+
+    # ______________________________________
+
     logging_print(logging=logging, line="STEP 3 : eval model, save preds ... ", end="")
-    scores_trn, scores_tst = eval_model(data_trn, data_tst, model_dirname=model_dir, model_fname=model_file)
+    scores_trn, scores_tst = eval_model(
+        data_trn, data_tst, model_dirname=model_dir, model_fname=model_file
+    )
     preds = model.predict(data_tst[data_tst.columns[:-1]])
     preds = pd.Series(preds)
     preds.to_csv("tmp_preds.csv")
 
     logging_print(logging=logging, line="OK")
-    logging_print(logging=logging, line='Train:' + str(scores_trn))
-    logging_print(logging=logging, line='Test:' + str(scores_tst))
-    
+    logging_print(logging=logging, line="Train:" + str(scores_trn))
+    logging_print(logging=logging, line="Test:" + str(scores_tst))
+
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("Current working directory:", os.getcwd())
-    if os.getcwd().split('/')[-1] != 'mipt-mlops-course':
-        print("Move to project directory...", end='')
-        os.chdir('./mipt-mlops-course')
+    if os.getcwd().split("/")[-1] != "mipt_mlops_project_1":
+        print("Move to project directory...", end="")
+        os.chdir("./mipt_mlops_project_1")
         print("OK")
         print("Current working directory:", os.getcwd())
 
